f22223d9554c264e66fffd0e7f405d8c167505fd
Lawrence Cherone <lcherone@users.noreply.github.com>
2020-06-24 20:35:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
-console.log(__dirname)
-
-app.set("views", __dirname+'/views')
+app.set("views", __dirname + '/views')
 app.set("view engine", "ejs")
 
 app.get('/', (req, res) => res.render('index', {
